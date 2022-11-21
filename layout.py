74ff31a6aaf755ab185620d76aaba91e2dddbd3a
mraro <32639415+mraro@users.noreply.github.com>
2022-11-21 08:33:50 -0300

    Update layout.py
    Layout traduz e retorna o valor ideal para maquina e para o usuario

--- layout.py
+++ layout.py
@@ -1,22 +1,30 @@
 import PySimpleGUI as GUI
+import NDIlib as ndi
 
 
 def layyout(args):
     GUI.theme('SandyBeach')
+    argos = []
+    rp = None
+    for s in args:
+        argos.append(s.ndi_name)
+    # combotext = 'teste','teste1','teste3'
+    # print(args)
+    # print(type(args))
+    # print(len(args))
 
-    #combotext = 'teste','teste1','teste3'
-    print(args)
-    print(type(args))
-    print(len(args))
-    layout = [[GUI.Combo(args)],
+    layout = [[GUI.InputCombo(argos, default_value=argos[0])],
+
               [GUI.Button('SALVA', font=('Times New Roman', 12)), GUI.Button('CANCELA', font=('Times New Roman', 12))]]
 
     win = GUI.Window("Entrada NDI", layout)
-
+    # print(win.read())
     e, v = win.read()
     if e == 'SALVA':
-        print(v[0])
+        argos.index(v[0])
+        print(v[0], "  ", argos.index(v[0]))
+        rp = args[argos.index(v[0])]
+        print(rp)
     win.close()
 
-
-#layyout('var1', 'var2','fasfasf')
+    return rp
